ddd6df489143c331c058febf798441d665ac2bb2
IsmaelRK <ismael2021ab@gmail.com>
2024-04-18 20:37:17 -0300

    save

--- SecondApp/SecondApp/src/HomeController.cs
+++ SecondApp/SecondApp/src/HomeController.cs
@@ -1,6 +1,27 @@
 namespace SecondApp;
 
-public class HomeController
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+public class HomeController : Controller
 {
-    
+
+    private readonly DatabaseConnection _context;
+
+    public HomeController(DatabaseConnection context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Calculate(double number1, double number2)
+    {
+        // Continue HERE
+        double result = num  
+    }
 }
