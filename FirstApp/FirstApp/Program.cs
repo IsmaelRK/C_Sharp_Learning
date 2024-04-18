d295ec0b8b442b76d5968eda235aba112d9b6b64
IsmaelRK <ismael2021ab@gmail.com>
2024-04-18 19:13:57 -0300

    save

--- FirstApp/FirstApp/Program.cs
+++ FirstApp/FirstApp/Program.cs
@@ -9,11 +9,14 @@ namespace SomeCalculator
         static void Main(string[] args)
         {
             Console.WriteLine("Average Calculator");
-            
-            
+
+            List<double> userEntriesList = new List<double>();
             
             for (int i = 1; i < 4; i++) {
+                
                 Console.Write($"Digit a Number ({i}): ");
+                double userEntries = Convert.ToDouble(Console.ReadLine());
+                userEntriesList.Add(userEntries);
             }
             
             
